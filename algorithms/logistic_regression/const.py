8ae74b3cb71d20c319f97579bdde886d448b68ec
FG-33 <fg.95@gmx.de>
2019-10-27 22:03:51 +0100

    Refractor project style: Add notebooks for lr and remove lr oo appoach/files.

--- algorithms/logistic_regression/const.py
+++
@@ -1,6 +0,0 @@
-_datasets_path = "../datasets/" # environment is algorithms/
-
-
-def MUSHROOMS_PATH():
-    """ Returns path to mushroom dataset relative to src/examples"""
-    return _datasets_path + "logistic_regression/mushrooms/agaricus-lepiota__processed.txt"
